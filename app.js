d1293db677e3306521d6b08333d9b221fb84c76e
Khudoyberdiyev <126224128+Khudoyberdiyev@users.noreply.github.com>
2024-05-22 09:38:58 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// let firstname = ["John", "Smith", "Bob", "Mike", "Nancy", "Alex", "Nicole", "John"];
+// firstname[6]= "Ra`no"
+// console.log(firstname);
+// console.log(firstname[firstname.length-1]);
+
+
+let weekDay = +prompt("Hafta Kunini kiriting: ");
+
+if (weekDay == Monday) {
+    alert("Dushanba")
+    
+}else if (weekDay == Sunday) {
+    alert("Seshanba")
+}else if (weekDay == Wednesday) {
+    alert("Chorshanba");
+}else{
+    alert("Hafta Kunini kiriting:");
+}
